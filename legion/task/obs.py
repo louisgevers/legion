11df72c183494862fbe6bc72eb694c35affe34cd
Louis Gevers <louis.gevers@gmail.com>
2026-02-05 11:55:06 +0100

    Implement common observation terms
    - q
    - dq
    - gravity_vector
    - base_linear_vel
    - base_angular_vel
    - foot_contacts

--- legion/task/obs.py
+++ legion/task/obs.py
@@ -57,3 +57,93 @@ class VelocityCommandObs:
     ) -> ArrayLike:
         vel_cmd = signals[0]
         return vel_cmd
+
+
+@register(OBSERVATIONS, "q")
+class JointPositionsObs:
+    name = "q"
+    required_signals = ()
+
+    def __init__(self, backend: Backend, embodiment: Embodiment, actuator: Actuator):
+        self.size = embodiment.n_joints
+
+    def __call__(
+        self, signals: tuple[ArrayLike, ...], sensor_data: SensorData
+    ) -> ArrayLike:
+        return sensor_data.q
+
+
+@register(OBSERVATIONS, "dq")
+class JointVelocitiesObs:
+    name = "dq"
+    required_signals = ()
+
+    def __init__(self, backend: Backend, embodiment: Embodiment, actuator: Actuator):
+        self.size = embodiment.n_joints
+
+    def __call__(
+        self, signals: tuple[ArrayLike, ...], sensor_data: SensorData
+    ) -> ArrayLike:
+        return sensor_data.dq
+
+
+@register(OBSERVATIONS, "gravity_vector")
+class GravityVectorObs:
+    name = "gravity_vector"
+    required_signals = ()
+
+    def __init__(self, backend: Backend, embodiment: Embodiment, actuator: Actuator):
+        self.backend = backend
+        self.size = 3
+        self.gravity_vector = self.backend.array([0, 0, -1])
+
+    def __call__(
+        self, signals: tuple[ArrayLike, ...], sensor_data: SensorData
+    ) -> ArrayLike:
+        vector = self.backend.quat_rotate(
+            sensor_data.base_quat, self.gravity_vector, inverse=True
+        )
+        norm = self.backend.norm(vector)
+        return vector / norm
+
+
+@register(OBSERVATIONS, "base_linear_vel")
+class BaseLinearVelocityObs:
+    name = "base_linear_vel"
+    required_signals = ()
+
+    def __init__(self, backend: Backend, embodiment: Embodiment, actuator: Actuator):
+        self.size = 3
+
+    def __call__(
+        self, signals: tuple[ArrayLike, ...], sensor_data: SensorData
+    ) -> ArrayLike:
+        return sensor_data.base_linear_vel
+
+
+@register(OBSERVATIONS, "base_angular_vel")
+class BaseAngularVelocityObs:
+    name = "base_angular_vel"
+    required_signals = ()
+
+    def __init__(self, backend: Backend, embodiment: Embodiment, actuator: Actuator):
+        self.size = 3
+
+    def __call__(
+        self, signals: tuple[ArrayLike, ...], sensor_data: SensorData
+    ) -> ArrayLike:
+        return sensor_data.base_angular_vel
+
+
+@register(OBSERVATIONS, "foot_contacts")
+class FootContactsObs:
+    name = "foot_contacts"
+    required_signals = ()
+
+    def __init__(self, backend: Backend, embodiment: Embodiment, actuator: Actuator):
+        self.size = embodiment.n_feet
+
+    def __call__(
+        self, signals: tuple[ArrayLike, ...], sensor_data: SensorData
+    ) -> ArrayLike:
+        return sensor_data.foot_contacts
