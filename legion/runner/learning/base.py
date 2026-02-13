2055ba8ccbd7b02b6bcca51511eed96385dcda97
Louis Gevers <louis.gevers@gmail.com>
2026-02-13 13:11:34 +0100

    Explicitly log rollout length in terms of time

--- legion/runner/learning/base.py
+++ legion/runner/learning/base.py
@@ -17,12 +17,16 @@ class LearningRunner(ABC):
         # Print some stats
         sim_steps = runtime._policy_decimation * self.n_iterations
         sim_duration = sim_steps * runtime.physics.dt
+        rollout_duration = self.rollout_length * runtime._policy_dt
         print(f"[{self.__class__.__name__}] initialized with:")
         print(
             f"- batch size:\t\t\t{self.batch_size:_} ({self.n_envs:_} envs x {self.rollout_length:_} steps)"
         )
         print(
             f"- learning iterations:\t\t{self.learning_iterations:_} ({self.n_iterations:_} policy steps)"
+        )
+        print(
+            f"- rollout length:\t\t{self.rollout_length} (real time: {rollout_duration:.2f}s)"
         )
         print(
             f"- simulation iterations:\t{sim_steps:_} (real time: {datetime.timedelta(seconds=int(sim_duration))})"
