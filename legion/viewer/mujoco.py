e2e186e24078b1b7ad9c9df36a43e992cfc62574
Louis Gevers <louis.gevers@gmail.com>
2026-02-13 11:04:25 +0100

    Make MuJoCo viewer keep an internal mujoco data
    This way when we reset it with other mujoco data it will still visualize normally

--- legion/viewer/mujoco.py
+++ legion/viewer/mujoco.py
@@ -7,18 +7,22 @@ from legion.physics.mujoco import MujocoPhysics, PhysicsState
 class MujocoViewer:
     def __init__(self, physics: MujocoPhysics):
         self._mj_model = physics._mj_model
-        self._mj_viewer: mujoco.viewer.Handle | None = None
-        self._first_run = True
+        self._mj_data = mujoco.MjData(self._mj_model)  # Internal data handle
+        self._mj_viewer = mujoco.viewer.launch_passive(self._mj_model, self._mj_data)
 
     def render(self, state: PhysicsState):
-        # Mujoco state data is just mutated
-        if self._mj_viewer is None:
-            self._mj_viewer = mujoco.viewer.launch_passive(self._mj_model, state.data)
-            self._first_run = False
+        # Copy into internal data
+        mujoco.mj_copyState(
+            self._mj_model,
+            state.data,
+            self._mj_data,
+            mujoco.mjtState.mjSTATE_INTEGRATION,  # Full state copy
+        )
+        mujoco.mj_forward(self._mj_model, self._mj_data)
         self._mj_viewer.sync()
 
     def is_running(self) -> bool:
-        return self._first_run or self._mj_viewer.is_running()
+        return self._mj_viewer.is_running()
 
     def close(self):
         self._mj_viewer.close()
