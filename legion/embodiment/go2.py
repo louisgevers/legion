dc2e3a8e5d756bbec22539e43daef9d344209fe8
Louis Gevers <louis.gevers@gmail.com>
2026-03-09 09:17:42 +0100

    Add missing type annotations in Go2 embodiment

--- legion/embodiment/go2.py
+++ legion/embodiment/go2.py
@@ -5,25 +5,25 @@ from legion.registry import EMBODIMENTS, register
 
 @register(EMBODIMENTS, "go2")
 class Go2(NamedTuple):
-    name = "go2"
-    n_joints = 12
-    n_actuators = 12
-    n_feet = 4
-    joint_names = tuple(
+    name: str = "go2"
+    n_joints: int = 12
+    n_actuators: int = 12
+    n_feet: int = 4
+    joint_names: tuple[str, ...] = tuple(
         [
             f"{leg}_{joint}_joint"
             for leg in ["FL", "FR", "RL", "RR"]
             for joint in ["hip", "thigh", "calf"]
         ]
     )
-    actuator_names = tuple(
+    actuator_names: tuple[str, ...] = tuple(
         [
             f"{leg}_{joint}"
             for leg in ["FL", "FR", "RL", "RR"]
             for joint in ["hip", "thigh", "calf"]
         ]
     )
-    feet_names = ("FL", "FR", "RL", "RR")
-    q_nominal = (0.0, 0.8, -1.5) * 2 + (0.0, 1.0, -1.5) * 2
-    base_xyz_init = (0.0, 0.0, 0.34)
-    total_mass = 15
+    feet_names: tuple[str, ...] = ("FL", "FR", "RL", "RR")
+    q_nominal: tuple[float, ...] = (0.0, 0.8, -1.5) * 2 + (0.0, 1.0, -1.5) * 2
+    base_xyz_init: tuple[float, float, float] = (0.0, 0.0, 0.34)
+    total_mass: float = 15.0
